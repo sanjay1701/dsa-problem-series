a962cc46ab87822ec80b400601161eb2810782f3
sanjay <kr.sanjay.1701@gmail.com>
2021-01-16 22:45:41 +0530

    feat: updated approach2 solution

--- DSAProblemSeries/src/array/RearrangeNegativetoBeginningandPositivetoEnd.java
+++ DSAProblemSeries/src/array/RearrangeNegativetoBeginningandPositivetoEnd.java
@@ -12,10 +12,10 @@ public class RearrangeNegativetoBeginningandPositivetoEnd {
 	 */
 	public static void rearrange(int[] arr, int n) {
 
-		//Approach- 1
+		//Approach- 1 (optimized approach)
 		int j=0;
 		for(int i=0;i<n;i++) {
-			if(arr[i]<0) {
+			if(arr[i]<=0) {
 				if(i!=j) {
 					int temp = arr[j];
 					arr[j] = arr[i];
@@ -28,17 +28,21 @@ public class RearrangeNegativetoBeginningandPositivetoEnd {
 			System.out.print(arr[i] + " ");
 		}
 
-		//Approach - 2 (only to print negative and positive to left and right respectively to maintain respective insertion orders)
-		/*
-		for(int j=0;j<n;j++) {
-			if(arr[j]<0) {
-				System.out.print(arr[j] + " ");}
+		//Approach - 2 (not optimized)
+		/*int count=0, lastIndex = arr.length;
+		for(int i = 0; i < n; i++) {
+			if(arr[i]<=0) {
+				arr[count++] = arr[i];
+			}
 		}
-		for(int k=0;k<n;k++) {
-			if(arr[k]>0) {
-				System.out.print(arr[k] + " ");}
+		for(int i = 0;i<lastIndex;i++) {
+			if(arr[i]>=0)
+				arr[count++] = arr[i];
 		}
-		 */	}
+		for(int i = 0; i < n; i++) {
+			System.out.print(arr[i]+ " ");
+		}*/
+	}
 	public static void main(String[] args) {
 		int arr[] = { -1, 2, -3, 4, 5, 6, -7, 8, 9 };
 		int n = arr.length;
