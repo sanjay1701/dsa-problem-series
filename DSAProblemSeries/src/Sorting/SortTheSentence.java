19e8acdaf9759c2f7af92ccd46e5f66375753358
sanjay1701 <kr.sanjay.1701@gmail.com>
2023-07-19 23:10:25 +0530

    Added approaches to solve Code for sorting the sentence

--- DSAProblemSeries/src/Sorting/SortTheSentence.java
+++ DSAProblemSeries/src/Sorting/SortTheSentence.java
@@ -9,7 +9,8 @@ import java.util.Comparator;
 public class SortTheSentence {
 
     public static String sortSentence(String s) {
-        String[] arr = s.split(" ");
+        //Approach 1 - Using Comparator
+        /*String[] arr = s.split(" ");
         Comparator<String> stringComparator = (o1, o2) -> {
             Integer num1 = Integer.parseInt(o1.substring(o1.length()-1));
             Integer num2 = Integer.parseInt(o2.substring(o2.length()-1));
@@ -25,6 +26,37 @@ public class SortTheSentence {
         }
         str = arr[i].replaceAll("\\d","");
         sb.append(str);
+        return sb.toString();*/
+
+        //Approach - 2 (using subString to find index and replaceAll methods to replace digit in array of string)
+        /*String[] arr = s.split(" ");
+        String[] newSentence = new String[arr.length];
+        StringBuilder sb = new StringBuilder();
+        int j ;
+        for(int i=0;i< arr.length;i++){
+            int index = Integer.parseInt(arr[i].substring(arr[i].length()-1));
+            newSentence[index-1] = arr[i].replaceAll("\\d","");
+        }
+        for(j=0;j< newSentence.length-1;j++){
+            sb.append(newSentence[j]+ " ");
+        }
+        sb.append(newSentence[j]);
+        return sb.toString();*/
+
+
+        //Approach - 3 (using chartAt to find index and substring to insert in new array)
+        String[] arr = s.split(" ");
+        String[] newSentence = new String[arr.length];
+        StringBuilder sb = new StringBuilder();
+        int i =0 ;
+        for (String word : arr) {
+            i = (int) (word.charAt(word.length() - 1) - '0');
+            newSentence[i - 1] = word.substring(0, word.length() - 1);
+        }
+        for(i=0;i< newSentence.length-1;i++){
+            sb.append(newSentence[i]+ " ");
+        }
+        sb.append(newSentence[i]);
         return sb.toString();
     }
 
